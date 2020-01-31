87bbed450a0bb20188dc590771ab7351a232b3ad
samir <wardag.as@gmail.com>
2020-01-31 22:54:22 +0300

    init

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(name='unictelecome',
       version='1.0',
-      url='#',
+      url='https://github.com/SamirWardak/sendmessageunictelecome',
       license='MIT',
       author='Wardak Ahmad Samir',
       author_email='wardag.as@gmail.com',
       description='Send sms from unictelecome.',
       packages=find_packages(exclude=['tests']),
+      download_url = 'https://github.com/SamirWardak/sendmessageunictelecome',
       long_description=open('README.md').read(),
       install_requires=[
             'requests',
